28760ae37fe169478e6f032c12077909a632a0fd
sonoppadon-andy <sonoppadon@gmail.com>
2026-09-21 19:43:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 window.CALLTREE_CONFIG = {
   clientId: "be89b11f-ca7d-4181-8a0a-20e8199ecc05",
   tenantId: "1f20664a-d62c-485d-8200-352c44549547",
-  redirectUri: "https://github.com/sonoppadon-andy/calltree-dashboard/",
+  redirectUri: "https://sonoppadon-andy.github.io/calltree-dashboard",
   sharePointHost: "singhaestate.sharepoint.com",
   sitePath: "/sites/ITProject",
   listName: "Member",
