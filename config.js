885e2be6d5506dcfb5770e2f88e27d7cfdb1190c
sonoppadon-andy <sonoppadon@gmail.com>
2026-09-21 19:28:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // Replace CLIENT_ID with the Application (client) ID from Microsoft Entra App Registration.
 // This file contains no client secret. Do not add a secret to a browser application.
 window.CALLTREE_CONFIG = {
-  clientId: "REPLACE_WITH_ENTRA_APPLICATION_CLIENT_ID",
+  clientId: "be89b11f-ca7d-4181-8a0a-20e8199ecc05",
   tenantId: "1f20664a-d62c-485d-8200-352c44549547",
-  redirectUri: "https://REPLACE_WITH_GITHUB_USERNAME.github.io/calltree-dashboard/",
+  redirectUri: "https://github.com/sonoppadon-andy/calltree-dashboard/",
   sharePointHost: "singhaestate.sharepoint.com",
   sitePath: "/sites/ITProject",
   listName: "Member",
